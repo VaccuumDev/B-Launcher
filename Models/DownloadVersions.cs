19a0febe3194eb7a339f9fc77d685e2a035bc6fd
Vac'cuum - FOSS <107546275+VaccuumDev@users.noreply.github.com>
2025-11-21 21:21:27 +0300

    Update

--- Models/DownloadVersions.cs
+++ Models/DownloadVersions.cs
@@ -4,58 +4,79 @@ using System.IO.Compression;
 using System.Threading.Tasks;
 using System.Net.Http;
 using Spamton;
+using DialogHostAvalonia;
+using Avalonia.Controls;
+using Downloader;
 
 namespace BW_Launcher.Models
 {
     public class VersionsInstaller
     {
+        private static void DownloadFileCompleted(string targetPath, string targetDir)
+        {
+            bool unpackOnLinux = false; // * idk some kind of debug
+
+            if (unpackOnLinux || MainWindowModel.osId != 1)
+            {
+                Log.Information("Unpacking...");
+                ZipFile.ExtractToDirectory(targetPath, targetDir);
+                Log.Information("Unpacked!");
+                DialogHost.Show(new TextBlock
+                {
+                    Text = $"Версия {targetPath} установлена!",
+                    FontSize = 18,
+                    Foreground = Avalonia.Media.Brushes.Green
+                });
+            }
+        }
+
         [BIG_SHOT]
         public static async Task<int> DownloadAsync(string url, string id)
         {
-            bool unpackOnLinux = false; // * idk some kind of debug
+            var downloadOpt = new DownloadConfiguration()
+            {
+                ChunkCount = 8, // Number of file parts, default is 1
+                ParallelDownload = true // Download parts in parallel (default is false)
+            };
             string targetDir = (MainWindowModel.osId == 1) ? "~/.b-world/versions/" : @"C:\Program Files\B-World\versions";
             string localFileName = id + ((MainWindowModel.osId == 1) ? ".AppImage" : ".zip");
-
             try
-      {
-        if (targetDir.StartsWith("~"))
-        {
-          string home = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-          targetDir = Path.Combine(home, targetDir.TrimStart('~', '/', '\\'));
-        }
-
-        if (!Directory.Exists(targetDir))
-          Directory.CreateDirectory(targetDir);
-
-        string targetPath = Path.Combine(targetDir, localFileName);
-        Log.Information($"Target path is {targetPath}");
-        Log.Information($"target directory is {targetDir}");
-
-        using var http = new HttpClient();
-        using var resp = await http.GetAsync(url, HttpCompletionOption.ResponseHeadersRead);
-        resp.EnsureSuccessStatusCode();
-
-        using var stream = await resp.Content.ReadAsStreamAsync();
-        using var fs = new FileStream(targetPath, FileMode.Create, FileAccess.Write, FileShare.None);
-
-        Task.Run(async () => await stream.CopyToAsync(fs))
-            .GetAwaiter().GetResult();
-        fs.Dispose();
-
-        Log.Information($"Saved raw file to {targetPath}");
-
-        if (unpackOnLinux || MainWindowModel.osId != 1)
-        {
-          Log.Information("Unpacking...");
-          ZipFile.ExtractToDirectory(targetPath, targetDir);
-          Log.Information("Unpacked!");
-        }
-
-        return 0;
-      }
-      catch (UnauthorizedAccessException)
             {
-                Log.Error("Access denied. Требуются права для записи в эту папку.");
+                if (targetDir.StartsWith("~"))
+                {
+                    string home = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+                    targetDir = Path.Combine(home, targetDir.TrimStart('~', '/', '\\'));
+                }
+
+                if (!Directory.Exists(targetDir))
+                    Directory.CreateDirectory(targetDir);
+
+                string targetPath = Path.Combine(targetDir, localFileName);
+                Log.Information($"Target path is {targetPath}");
+                Log.Information($"target directory is {targetDir}");
+
+                using var http = new HttpClient();
+                using var resp = await http.GetAsync(url, HttpCompletionOption.ResponseHeadersRead);
+                resp.EnsureSuccessStatusCode();
+
+                Log.Information($"Response content length: {resp.Content.Headers.ContentLength} bytes");
+
+                var downloader = new DownloadService(downloadOpt);
+                await downloader.DownloadFileTaskAsync(url, targetPath);
+                DownloadFileCompleted(targetPath, targetDir);
+                /*using var fs = new FileStream(targetPath, FileMode.Create, FileAccess.Write, FileShare.None);
+                await resp.Content.CopyToAsync(fs).ContinueWith(
+                    (copytask) => {
+                      fs.Dispose(); 
+                      Log.Information($"Saved raw file to {targetPath}"); 
+                      DownloadFileCompleted(targetPath, targetDir);
+                    });*/
+                //fs.Dispose();
+                return 0;
+            }
+            catch (UnauthorizedAccessException)
+            {
+                Log.Error("Access denied.");
                 return 2;
             }
             catch (Exception ex)
--- Models/MainWindowModel.cs
+++ Models/MainWindowModel.cs
@@ -1,5 +1,4 @@
-using System;
-using BW_Launcher;
+
 using System.Collections.Generic;
 using Spamton;
 using System.Runtime.InteropServices;
@@ -7,73 +6,73 @@ using System.Collections.ObjectModel;
 
 namespace BW_Launcher.Models
 {
-    public class MainWindowModel
+  public class MainWindowModel
+  {
+    const string JsonURL = "http://192.168.1.8:8080/versions.json"; // TODO ПОДНЯТЬ СЕРВАК
+    public static sbyte osId { get; } = GetOS();
+
+    /*[BIG_SHOT]
+    static BW_Launcher.Models.Version GetVersion()
     {
-        const string JsonURL = "http://192.168.1.8:8080/versions.json"; // TODO ПОДНЯТЬ СЕРВАК
-        public static sbyte osId { get; } = GetOS();
+        return JSONProcesser.GetRemoteJsonAsync(JsonURL).Result;
+    }*/
 
-        /*[BIG_SHOT]
-        static BW_Launcher.Models.Version GetVersion()
-        {
-            return JSONProcesser.GetRemoteJsonAsync(JsonURL).Result;
-        }*/
+    public static List<BW_Launcher.Models.Version> versionsList = GetVersionsList();
 
-        public static List<BW_Launcher.Models.Version> versionsList = GetVersionsList();
+    [BIG_SHOT]
+    public static List<BW_Launcher.Models.Version> GetVersionsList()
+    {
+      List<BW_Launcher.Models.Version> output = JSONProcesser.GetRemoteJsonAsync(JsonURL).Result;
 
-        [BIG_SHOT]
-        public static List<BW_Launcher.Models.Version> GetVersionsList()
-        {
-            List<BW_Launcher.Models.Version> output = JSONProcesser.GetRemoteJsonAsync(JsonURL).Result;
+      ////for (byte i=0;i<versionsAmount;i++) тут надо шото нормальное делатии, а то не робит
+      ////{
+      ////    output[i] = GetVersion();
+      ////}
 
-            ////for (byte i=0;i<versionsAmount;i++) тут надо шото нормальное делатии, а то не робит
-            ////{
-            ////    output[i] = GetVersion();
-            ////}
-
-            if (output == null)
-            {
-                Log.Information("Versions list is null", "ERROR");
-                return new List<BW_Launcher.Models.Version>();
-            }
-            else
-                Log.Information($"versions count: {output.Count}", "DEBUG");
-                return output;
-        }
-
-        [BIG_SHOT]
-        public static ObservableCollection<string> GetVersionsDisplayNames()
-        {
-            ObservableCollection<string> output = new ObservableCollection<string>();
-
-            for (byte i=0;i<versionsList.Count;i++)
-            {
-                output.Add(versionsList[i].name);
-            }
-            
-            Log.Information($"Latest version is {output[0]}", "FUN");
-
-            return output;
-            //return new ObservableCollection<string>() {"DEBUG"};
-        }
-
-        /*
-            1 - Linux
-            0 - Windows
-        */
-        [BIG_SHOT]
-        public static sbyte GetOS()
-        {
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-            {
-                Log.Information("Linux user detected!");
-                return 1;
-            }
-            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-            {
-                Log.Information("Microsoft Spyware user detected!");
-                return 0;
-            }
-            else return -1;
-        }
+      if (output == null)
+      {
+        Log.Error("Versions list is null");
+        return new List<BW_Launcher.Models.Version>();
+      }
+      else
+        Log.Debug($"versions count: {output.Count}");
+      return output;
     }
+
+    [BIG_SHOT]
+    public static ObservableCollection<string> GetVersionsDisplayNames()
+    {
+      ObservableCollection<string> output = new ObservableCollection<string>();
+
+      for (byte i = 0; i < versionsList.Count; i++)
+      {
+        output.Add(versionsList[i].name);
+      }
+
+      Log.Information($"Latest version is {output[0]}");
+
+      return output;
+      //return new ObservableCollection<string>() {"DEBUG"};
+    }
+
+    /*
+        1 - Linux
+        0 - Windows
+    */
+    [BIG_SHOT]
+    public static sbyte GetOS()
+    {
+      if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+      {
+        Log.Information("Linux user detected!");
+        return 1;
+      }
+      else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+      {
+        Log.Information("Microsoft Spyware user detected!");
+        return 0;
+      }
+      else return -1;
+    }
+  }
 }
